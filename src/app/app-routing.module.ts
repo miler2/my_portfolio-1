680b118f8bca35370e330a1509c4364365f326b1
miler2 <vegavegewtu@gmail.com>
2026-03-07 10:14:14 +0100

    I have made the first navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Components
+import { CvComponent } from './components/cv/cv.component';
+import { LandpageComponent } from './components/landpage/landpage.component';
+
+const routes: Routes = [
+  { path: '', component: LandpageComponent},
+  { path: 'cv', component: CvComponent},
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CvComponent } from './components/cv/cv.component';
+import { LandpageComponent } from './components/landpage/landpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CvComponent,
+    LandpageComponent,
   ],
   imports: [
     BrowserModule,
